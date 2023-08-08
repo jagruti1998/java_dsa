af25c9ae8b1ab68ca56ee0648df2f6f8d0cd9dc6
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-08 17:57:03 +0530

    commit

---
+++ src/stack/MyStack.java
@@ -0,0 +1,48 @@
+package stack;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q2.offer(x);
+
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+
+        // Swap the names of the queues to maintain the invariant
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return q1.poll();
+    }
+
+    public int top() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return q1.peek();
+    }
+
+    public boolean isEmpty() {
+        return q1.isEmpty();
+    }
+}
+
