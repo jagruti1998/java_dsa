d3dbd10ddeea9169a1c12c1ba2fe2f47981bef6b
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-11 16:00:17 +0530

    commit

---
+++ src/arrays/singleNonDuplicate.java
@@ -0,0 +1,43 @@
+package arrays;
+import java.util.Scanner;
+
+public class singleNonDuplicate {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        singleNonDuplicate solution = new singleNonDuplicate();
+        int result = solution.singleNonDuplicate(nums);
+        System.out.println("The single non-duplicate element is: " + result);
+    }
+}
