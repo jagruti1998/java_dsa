0c4df4dae849634136059cd312d5889bd15d9b51
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-09-02 13:15:25 +0530

    RemoveDuplicates

--- src/arrays/RemoveDuplicates.java
+++ src/arrays/RemoveDuplicates.java
@@ -1,26 +1,43 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class RemoveDuplicates {
 
-        public static void main(String[] args) {
-            int[] arr = {1, 1, 2, 2, 3, 4, 5, 5, 6};
-            int length = removeDuplicates(arr);
-            System.out.println("Length after removing duplicates: " + length);
-            System.out.print("Array without duplicates: ");
-            for (int i = 0; i < length; i++) {
-                System.out.print(arr[i] + " ");
-            }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        public static int removeDuplicates(int[] arr) {
-            int i = 0;
-            for (int j = 1; j < arr.length; j++) {
-                if (arr[i] != arr[j]) {
-                    arr[i + 1] = arr[j];
-                    i++;
-                }
-            }
-            return i + 1;
+        int length = removeDuplicates(arr);
+        System.out.println("Length after removing duplicates: " + length);
+        System.out.print("Array without duplicates: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr[i] + " ");
         }
+
+        scanner.close();
     }
 
+    public static int removeDuplicates(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                arr[i + 1] = arr[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+}
