4d3f0d88e5e0d8bdbd604079a0ff30f87a9f37ea
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-09-18 23:05:16 +0530

    RightRotateArray

--- src/arrays/RightRotateArray.java
+++ src/arrays/RightRotateArray.java
@@ -7,7 +7,9 @@ public class RightRotateArray {
         public void rotate(int[] nums, int k) {
             k %= nums.length;
             reverse(nums, 0, nums.length - 1);
+            System.out.println(Arrays.toString(nums));
             reverse(nums, 0, k - 1);
+            System.out.println(Arrays.toString(nums));
             reverse(nums, k, nums.length - 1);
         }
 
