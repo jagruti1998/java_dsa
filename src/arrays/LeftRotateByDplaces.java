7efcc524f50616317333452ae81f72190e3b15ea
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-09-18 23:04:42 +0530

    leftRotateArrayByOne

--- src/arrays/LeftRotateByDplaces.java
+++ src/arrays/LeftRotateByDplaces.java
@@ -1,4 +1,30 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class LeftRotateByDplaces {
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 2;
+        new LeftRotateByDplaces().rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
 }
