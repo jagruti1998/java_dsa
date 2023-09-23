d7605471986cd92c227f15db724c36e1e2327cff
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-09-23 13:50:30 +0530

    MaxSubArray

---
+++ src/arrays/MaxSubArray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class MaxSubArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int maxSum = maxSubArray1(nums);
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+    }
+
+    public static int maxSubArray1(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
