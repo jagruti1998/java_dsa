2247d01a524dd5f6fbd4da8aa080791f814fe338
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-10-01 17:44:31 +0530

    commit

--- src/arrays/ReverseWordString.java
+++ src/arrays/ReverseWordString.java
@@ -1,4 +1,39 @@
 package arrays;
 
 public class ReverseWordString {
+    public static void main(String[] args) {
+        // Example input string
+        String input = "Hello World";
+
+        // Call the reverseWords method
+        String reversedString = reverseWords(input);
+
+        // Print the reversed string
+        System.out.println(reversedString);
+    }
+
+    public static String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+        int start = 0;
+        int end = 0;
+
+        while (start < arr.length) {
+            while (end < arr.length && arr[end] != ' ') {
+                end++;
+            }
+
+            reverse(start, end - 1, arr);
+            start = end + 1;
+            end = start;
+        }
+        return new String(arr);
+    }
+
+    private static void reverse(int l, int r, char[] arr) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
 }
